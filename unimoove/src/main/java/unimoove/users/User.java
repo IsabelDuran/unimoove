dcdfd60807b5f5cdbc936d1ed7c33934d3128de5
IsabelDuran <isabel.duranchumillas@alum.uca.es>
2020-03-14 01:57:40 +0100

    Version simple de usuario para empezar

--- unimoove/src/main/java/unimoove/users/User.java
+++ unimoove/src/main/java/unimoove/users/User.java
@@ -1,5 +1,15 @@
 package unimoove.users;
 
-public class User {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private String lastname;
 }
--- unimoove/src/main/java/unimoove/users/UsersRepository.java
+++ unimoove/src/main/java/unimoove/users/UsersRepository.java
@@ -1,8 +1,9 @@
 package unimoove.users;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UsersRepository {
+public interface UsersRepository extends CrudRepository<User, Long> {
 
 }
